f4684b9a473aea9dc0983f10e67cee9ae8e74a78
Motaz <102158878+TrMotaz@users.noreply.github.com>
2022-06-21 13:43:57 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import NavBar from './Components/NavBar';
+import {Route,Routes} from 'react-router-dom';
+import Home from './Components/Home';
+import UsersList from './Components/UserList';
+import Profile from './Components/Profile';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar/>
+      <Routes>
+      <Route path="/" element={<Home/>}  />
+      <Route path="/users" element={<UsersList/>} />
+      <Route path="/profile/: idUser" element={<Profile/> }  />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
